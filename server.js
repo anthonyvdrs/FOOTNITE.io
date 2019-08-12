fbf0abe80595849d9de289c6d27bb619ad4f0437
Anthony Vandersmissen <anthony.vandersmissen1@gmail.com>
2019-08-12 15:19:39 +0200

    speed

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const port = 3100;
+const port = 3000;
 const express = require('express');
 const app = express();
 const server = app.listen(port, () => console.log(`Server running on port ${port}`)); // listen on port 3000
