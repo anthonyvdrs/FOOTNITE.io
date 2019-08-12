3419d2ad29024922eb706ae19462b7a0a58d59d6
Balthazar Delvaux <delvaux.balthazar@gmail.com>
2019-08-12 14:58:32 +0200

    add movement

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,39 +1,70 @@
 const config = {
-    type: Phaser.AUTO,
-    width: 600,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-  };
-  
-  const game = new Phaser.Game(config);
-  
-  function preload() {
-    this.load.image('fond', 'assets/image/FootNite-proto600x600.png');
-    this.load.image('ballon', 'assets/image/ballon45x45.png');
-    this.load.image('blue', 'assets/image/playerBlue38x38.png');
-    this.load.image('purple', 'assets/image/playerPurple38x38.png');
-    this.load.image('red', 'assets/image/playerRed38x38.png');
-    this.load.image('yellow', 'assets/image/playerYellow38x38.png');
-    
-  } 
+  type: Phaser.AUTO,
+  width: 600,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {}
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
 
-  function create() {
-    console.log(this)
-    this.add.image(300, 300, 'fond');
-    this.add.image(300, 300, 'ballon');
-    this.add.image(450, 150, 'blue');
-    this.add.image(450, 450, 'purple');
-    this.add.image(150, 150, 'red');
-    this.add.image(150, 450, 'yellow');
+const game = new Phaser.Game(config);
+const players = [];
+let ballon = '';
+let cursors = '';
+
+function preload() {
+  this.load.image('fond', 'assets/image/FootNite-proto600x600.png');
+  this.load.image('ballon', 'assets/image/ballon45x45.png');
+  this.load.image('blue', 'assets/image/playerBlue38x38.png');
+  this.load.image('purple', 'assets/image/playerPurple38x38.png');
+  this.load.image('red', 'assets/image/playerRed38x38.png');
+  this.load.image('yellow', 'assets/image/playerYellow38x38.png');
+
+}
+
+function create() {
+  this.add.image(300, 300, 'fond');
+  ballon = this.physics.add.image(300, 300, 'ballon');
+  players[0] = this.physics.add.image(450, 150, 'blue');
+  players[1] = this.physics.add.image(450, 450, 'purple');
+  players[2] = this.physics.add.image(150, 150, 'red');
+  players[3] = this.physics.add.image(150, 450, 'yellow');
+
+  // enable world collision for ballon and players
+  for (const el of players) {
+    el.body.collideWorldBounds = true;
   }
-  
-  function update() {
-    
+  ballon.body.collideWorldBounds = true;
+
+  //setup cursors
+  cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+  //stop velocity when not pressing buttons
+  players[1].setVelocity(0, 0);
+
+  //set velocity for each direction
+  if (cursors.up.isDown) {
+    players[1].setVelocity(0, -100);
   }
+  if (cursors.down.isDown) {
+    players[1].setVelocity(0, 100);
+  }
+  if (cursors.left.isDown) {
+    players[1].setVelocity(-100, 0);
+  }
+  if (cursors.right.isDown) {
+    players[1].setVelocity(100, 0);
+  }
+
+}
 
 
 ///////////////
