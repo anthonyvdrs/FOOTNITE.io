0f8331f5d2e6ff3bc37e89ccfa2543fac1761257
Balthazar Delvaux <delvaux.balthazar@gmail.com>
2019-08-13 12:20:57 +0200

    upd

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -24,6 +24,8 @@ const gameScene = () => {
   const players = {};
   let ballon = {};
   let cursors = '';
+  let ball;
+  let player;
 
   function preload() {
     this.load.image('fond', 'assets/image/FootNite-proto600x600.png');
@@ -34,8 +36,8 @@ const gameScene = () => {
   function create() {
     this.add.image(300, 300, 'fond');
     this.matter.world.setBounds(0, 0, 600, 600, 32, true, true, false, true);
-    let ball = this.matter.add.image(300, 300, 'ballon');
-    let player = this.matter.add.image(100, 100, 'blue');
+    ball = this.matter.add.image(300, 300, 'ballon');
+    player = this.matter.add.image(100, 100, 'blue');
     ball.setCircle();
     ball.setFriction(0.005);
     ball.setBounce(1);
@@ -44,7 +46,22 @@ const gameScene = () => {
 
 
   function update() {
-    
+    //stop velocity when not pressing buttons
+    let x = 0;
+    let y = 0;
+    player.setVelocity(x, y);
+
+    //set velocity for each direction
+    if (cursors.left.isDown) {
+      player.setVelocity(x -= 200, y);
+    } else if (cursors.right.isDown) {
+      player.setVelocity(x += 200, y);
+    }
+    if (cursors.up.isDown) {
+      player.setVelocity(x, y -= 200);
+    } else if (cursors.down.isDown) {
+      player.setVelocity(x, y += 200);
+    }
   }
 }
 gameScene();
