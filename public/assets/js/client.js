ea4484bf81b6a9470df6a2c342aabd7a14c6cc59
Balthazar Delvaux <delvaux.balthazar@gmail.com>
2019-08-13 12:12:29 +0200

    initial settings

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -4,9 +4,12 @@ const gameScene = () => {
     width: 600,
     height: 600,
     physics: {
-      default: 'arcade',
-      arcade: {
-        debug: true
+      default: 'matter',
+      matter: {
+        debug: true,
+        gravity: {
+          y: 0
+        }
       }
     },
 
@@ -18,7 +21,7 @@ const gameScene = () => {
   };
 
   const game = new Phaser.Game(config);
-  const players = [];
+  const players = {};
   let ballon = {};
   let cursors = '';
 
@@ -26,75 +29,22 @@ const gameScene = () => {
     this.load.image('fond', 'assets/image/FootNite-proto600x600.png');
     this.load.image('ballon', 'assets/image/ballon45x45.png');
     this.load.image('blue', 'assets/image/playerBlue38x38.png');
-    this.load.image('purple', 'assets/image/playerPurple38x38.png');
-    this.load.image('red', 'assets/image/playerRed38x38.png');
-    this.load.image('yellow', 'assets/image/playerYellow38x38.png');
-
   }
 
   function create() {
     this.add.image(300, 300, 'fond');
-    ballon = this.physics.add.image(300, 300, 'ballon');
-    players[0] = this.physics.add.image(450, 150, 'blue');
-    players[1] = this.physics.add.image(450, 450, 'purple');
-    players[2] = this.physics.add.image(150, 150, 'red');
-    players[3] = this.physics.add.image(150, 450, 'yellow');
-
-    // enable world collision for ballon and players
-    for (const el of players) {
-      el.body.collideWorldBounds = true;
-    }
-    ballon.body.collideWorldBounds = true;
-    ballon.body.bounce.setTo(1, 1);
-
-    //setup collision
-    this.physics.add.collider(players, ballon);
-    this.physics.add.collider(players, players);
-
-    ballon.body.setAllowDrag(true);
-    ballon.body.setDrag(100, 100);
-    ballon.body.setFriction(0.5, 0.5);
-    ballon.body.setCircle(22, 0, 0);
-    players[0].body.setCircle(19);
-    players[1].body.setCircle(19);
-    players[2].body.setCircle(19);
-    players[3].body.setCircle(19);
-
-    //setup cursors
-    cursors = this.input.keyboard.createCursorKeys();
-
-    //setup collision
-    this.physics.add.collider(players, ballon);
-    this.physics.add.collider(players, players);
-
-    //setup friction
-    ballon.body.setAllowDrag(true);
-    ballon.body.setDrag(200, 200);
-    ballon.body.setFriction(0.5, 0.5);
-
-    // hitbox ballon
-    ballon.setSize(25, 25, true);
+    this.matter.world.setBounds(0, 0, 600, 600, 32, true, true, false, true);
+    let ball = this.matter.add.image(300, 300, 'ballon');
+    let player = this.matter.add.image(100, 100, 'blue');
+    ball.setCircle();
+    ball.setFriction(0.005);
+    ball.setBounce(1);
+    player.setCircle();
   }
 
 
   function update() {
-    //stop velocity when not pressing buttons
-    let x = 0;
-    let y = 0;
-    players[1].setVelocity(x, y);
-
-    //set velocity for each direction
-    if (cursors.left.isDown) {
-      players[1].setVelocity(x -= 200, y);
-    } else if (cursors.right.isDown) {
-      players[1].setVelocity(x += 200, y);
-    }
-    if (cursors.up.isDown) {
-      players[1].setVelocity(x, y -= 200);
-    } else if (cursors.down.isDown) {
-      players[1].setVelocity(x, y += 200);
-    }
-
+    
   }
 }
 gameScene();
